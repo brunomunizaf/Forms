1f62022382e3dbbe453f323ef722122a457dc098
Bruno Muniz Azevedo Filho <bruno@elixxir.io>
2023-09-26 14:01:10 -0300

    Implement init by coder to use xib

--- Sources/Forms/FormView.swift
+++ Sources/Forms/FormView.swift
@@ -21,6 +21,25 @@ public final class FormView: UIView {
     self.elements = elements
     super.init(frame: .zero)
 
+    setupStackView()
+    elements.forEach {
+      stackView.addArrangedSubview($0)
+      stackView.setCustomSpacing($0.spacingAfter, after: $0)
+    }
+  }
+
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    setupStackView()
+  }
+
+  public func add<T>(_ element: T) where T: FormItem {
+    elements.append(element)
+    stackView.addArrangedSubview(element)
+    stackView.setCustomSpacing(element.spacingAfter, after: element)
+  }
+
+  private func setupStackView() {
     stackView.axis = .vertical
     stackView.translatesAutoresizingMaskIntoConstraints = false
     addSubview(stackView)
@@ -32,18 +51,5 @@ public final class FormView: UIView {
       stackView.bottomAnchor.constraint(equalTo: bottomAnchor),
       stackView.widthAnchor.constraint(equalTo: widthAnchor)
     ])
-
-    elements.forEach {
-      stackView.addArrangedSubview($0)
-      stackView.setCustomSpacing($0.spacingAfter, after: $0)
-    }
-  }
-
-  required init?(coder: NSCoder) { nil }
-
-  public func add<T>(_ element: T) where T: FormItem {
-    elements.append(element)
-    stackView.addArrangedSubview(element)
-    stackView.setCustomSpacing(element.spacingAfter, after: element)
   }
 }
